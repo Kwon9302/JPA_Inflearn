a8bad9db3b178b4236f6af879304f7d924d76e55
Kwon9302 <148173045+Kwon9302@users.noreply.github.com>
2024-08-03 10:54:54 +0900

    240803 영속성 시작

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,33 @@
+package hellojpa;
+
+import jakarta.persistence.*;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+        try {
+            // 비영속
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("Hello JPA");
+
+            // 영속
+            System.out.println("Before");
+            em.persist(member);
+            System.out.println("After");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
